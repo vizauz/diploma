eeee71f980b3a9233cfcacf9317047f023de92f5
Danil <ppxy@ya.ru>
2016-02-03 14:26:20 +0300

    Added create group functional

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class AddUser(Form):
+	username = StringField('User name', validators=[DataRequired('Enter user name')])
+	email = StringField('User email', validators=[DataRequired('Enter email'), Email('Enter valid eamil addres')])
+	submit = SubmitField('Add user')
